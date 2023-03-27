f10e24a873357d9b84b07114cf7aab075a792178
John Eric <johneric.engana@g.msuiit.edu.ph>
2023-03-27 21:25:56 +0800

    Finished is_match function

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,5 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    fave_numbers_1_set = set(fave_numbers_1)
+    return all(number in fave_numbers_1_set for number in fave_numbers_2)
