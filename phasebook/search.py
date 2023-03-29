33929709005078e9ffcf6deaaf7545317be52e2c
John Eric <johneric.engana@g.msuiit.edu.ph>
2023-03-30 01:55:31 +0800

    search.py partially done. Missing task: searching users with multiple parameters

--- phasebook/search.py
+++ phasebook/search.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-
+import re 
 from .data.search_data import USERS
 
 
@@ -9,6 +9,7 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 @bp.route("")
 def search():
     return search_users(request.args.to_dict()), 200
+
 
 
 def search_users(args):
@@ -24,7 +25,28 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
+    # Create a list to store the matching users
+    matches = []
 
-    # Implement search here!
+    # Loop through the users and check if they match the search criteria
+    for user in USERS:
+        matched = True
+        for arg, value in args.items():
+            if arg == "id" and value != user["id"]:
+                matched = False
+                break
+            if arg == "name" and value.lower() not in user["name"].lower():
+                matched = False
+                break
+            if arg == "age":
+                age = int(value)
+                if not (age - 1 <= user["age"] <= age + 1):
+                    matched = False
+                    break
+            if arg == "occupation" and value.lower() not in user["occupation"].lower():
+                matched = False
+                break
+        if matched:
+            matches.append(user)
 
-    return USERS
+    return matches
